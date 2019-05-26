60e7d02685707bfde2ec07bfe1f0158564fe858a
sajicode <sajioloye@gmail.com>
2019-05-26 08:00:19 +0100

    setup user model

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+const { Schema } = require('mongoose');
+
+// Define our model
+
+const userSchema = new Schema({
+	email: {
+		type: String,
+		unique: true,
+		lowercase: true
+	},
+	password: String
+});
+// Create the model class
+const ModelClass = mongoose.model('user', userSchema);
+
+// Export the model
+module.exports = ModelClass;
