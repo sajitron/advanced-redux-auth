52c30848039b174e440619461edab74cb31a2ea7
sajicode <sajioloye@gmail.com>
2019-05-26 07:49:28 +0100

    setup basic route

---
+++ server/index.js
@@ -0,0 +1,21 @@
+// starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const router = require('./router');
+
+// instantiate express
+const app = express();
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+});
