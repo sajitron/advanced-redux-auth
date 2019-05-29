92c0e96c3d29c114b92fc6eb4093d54ed8091f25
sajicode <sajioloye@gmail.com>
2019-05-29 09:14:52 +0100

    handle form submit errors, redirect on signup

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -7,7 +7,10 @@ import * as actions from '../../actions';
 class Signup extends Component {
 	onSubmit = (formProps) => {
 		// formProps contains all the items a user inputs
-		this.props.signup(formProps);
+		this.props.signup(formProps, () => {
+			this.props.history.push('/feature');
+			// redirect on submit
+		});
 	};
 
 	render() {
@@ -23,13 +26,18 @@ class Signup extends Component {
 					<label>Password</label>
 					<Field name="password" type="password" component="input" autoComplete="none" />
 				</fieldset>
+				<div>{this.props.errorMessage}</div>
 				<button>Sign Up!</button>
 			</form>
 		);
 	}
 }
 
-export default compose(connect(null, actions), reduxForm({ form: 'signup' }))(Signup);
+function mapStateToProps(state) {
+	return { errorMessage: state.auth.errorMessage };
+}
+
+export default compose(connect(mapStateToProps, actions), reduxForm({ form: 'signup' }))(Signup);
 
 // we pass null as the first value because we have no state object to pass
 
